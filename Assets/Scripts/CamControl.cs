81081782b3e6f6434088c745b66060eb173a78a4
kn4ughty <Kn4ughty@users.noreply.github.com>
2023-05-20 19:44:35 +1000

    Basic camera zoom

--- Assets/Scripts/CamControl.cs
+++ Assets/Scripts/CamControl.cs
@@ -8,14 +8,31 @@ public class CamControl : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
+    
+    public Camera m_OrthographicCamera;
+    public bool ScrollToZoom;
+    public float ScrollSensitivity;
+
+    private void Update() {
+       
+    }
+
     private void LateUpdate()
     {
         if (target == null)
             return;
+        
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f ) // forward
+        {
+            m_OrthographicCamera.orthographicSize += Input.GetAxis("Mouse ScrollWheel");
+        }
+
 
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
     }
+
+
 
 }
