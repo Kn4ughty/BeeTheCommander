f29cd6549dbcf62f73bb75482109f537b6609926
kn4ughty <Kn4ughty@users.noreply.github.com>
2023-05-22 11:48:23 +1000

    Comments and cleanup
    Explained some functions, removed leftover code from experimenting.

--- Assets/Scripts/Interaction/Highlight.cs
+++ Assets/Scripts/Interaction/Highlight.cs
@@ -6,7 +6,7 @@ public class Highlight : MonoBehaviour
 {
     public Color highlightColour = Color.green;
     public float transitionTime = 1f;
-    public float highlightBrightness = 1.5f; // Adjust this value to control the brightness
+    public float highlightBrightness = 1.5f; // not in use
 
     public bool isHighlighted;
     private bool m_isHighlighted = false;
@@ -23,16 +23,8 @@ public class Highlight : MonoBehaviour
 
     private void Update()
     {
-        /*
-        if (isHighlighted)
-        {
-            transitionCoroutine = StartCoroutine(TransitionColor(highlightColour));
-        }
-        else 
-        {
-            transitionCoroutine = StartCoroutine(TransitionColor(startColor));
-        }
-        */
+        // This mess makes it so that the code below only runs when the var changes
+        // This is done so that i dont remember but its important trust
         if (isHighlighted != m_isHighlighted && OnVariableChange != null)
         {
             m_isHighlighted = isHighlighted;
@@ -48,6 +40,8 @@ public class Highlight : MonoBehaviour
         }
     }
 
+    // works without using thjis
+    // might be usefulk?
     void HandleVariableChange(bool newVal)
     {
     // Handle the variable change here
@@ -63,16 +57,15 @@ public class Highlight : MonoBehaviour
         startColor = objectRenderer.material.color;
         OnVariableChange += HandleVariableChange;
     }
-    // hellooooo i love youuuuuuu pretty girlll
+    // hellooooo
+
 
     private void OnMouseEnter()
     {
-
         if (transitionCoroutine != null)
             StopCoroutine(transitionCoroutine);
 
         isHighlighted = true;
-
     }
 
     private void OnMouseExit()
@@ -80,27 +73,8 @@ public class Highlight : MonoBehaviour
         if (transitionCoroutine != null)
             StopCoroutine(transitionCoroutine);
 
-        //transitionCoroutine = StartCoroutine(TransitionColor(startColor));
         isHighlighted = false;
     }
-    /*
-    private void OnMouseEnter()
-    {
-
-        if (transitionCoroutine != null)
-            StopCoroutine(transitionCoroutine);
-
-        transitionCoroutine = StartCoroutine(TransitionColor(highlightColour));
-    }
-
-    private void OnMouseExit()
-    {
-        if (transitionCoroutine != null)
-            StopCoroutine(transitionCoroutine);
-
-        transitionCoroutine = StartCoroutine(TransitionColor(startColor));
-    }
-    */
 
     private IEnumerator TransitionColor(Color targetColor)
     {
@@ -114,19 +88,6 @@ public class Highlight : MonoBehaviour
             elapsedTime += Time.deltaTime;
             yield return null;
             
-
-            /*
-            // Calculate the interpolated color
-            Color lerpedColor = Color.Lerp(currentColor, targetColor * highlightBrightness, elapsedTime / transitionTime);
-            
-            // Apply brightness adjustment
-            Color adjustedColor = lerpedColor; //* highlightBrightness;
-            
-            objectRenderer.material.color = adjustedColor;
-            
-            elapsedTime += Time.deltaTime;
-            yield return null;
-            */
         }
 
         objectRenderer.material.color = targetColor;
@@ -135,20 +96,4 @@ public class Highlight : MonoBehaviour
     }
 
 
-
-/* My code
-
-    void OnMouseEnter() {
-        startcolor = GetComponent<Renderer>().material.color;
-        
-        Color desiredColor = HighlightColour;
-        Color smoothedColor = Color.Lerp(GetComponent<Renderer>().material.color, HighlightColour, TransitionTime);
-
-        GetComponent<Renderer>().material.color = smoothedColor;
-    }
-
-    void OnMouseExit() {
-        GetComponent<Renderer>().material.color = startcolor;
-    }
-    */
 }
--- Assets/Scripts/Interaction/Interactor.cs
+++ Assets/Scripts/Interaction/Interactor.cs
@@ -15,31 +15,33 @@ public class Interactor : MonoBehaviour
     private Collider2D LastInteractable = null; // help
 
 
-    // Update is called once per frame
     void Update()
     {
-       //_numFound = Physics2D.OverlapCircle(_interactionPoint.position, _interactionRadius, _colliders, _interactableMask); 
+       // Gets an array of objects in the interaction radius
         Collider2D[] colliders = Physics2D.OverlapCircleAll(_interactionPoint.position, _interactionRadius, _interactableMask);
-        _numFound = colliders.Length;
-        //_colliders = colliders;
+
+        _numFound = colliders.Length; // Used for debugging
 
         if (_numFound > 0)
         {
-
+            // Gets interactable thing
             var interactable = colliders[0].GetComponent<IInteractable>();
 
+            // Sets and gets the highlting variable
             highlight = colliders[0].GetComponent<Highlight>();
             highlight.isHighlighted = true;
 
 
             if (interactable != null && Input.GetKeyDown("e"))
             {
+                // Runs the interact function
                 interactable.Interact(this);
             }
             
+            // this is used so that the highlighting can be turned off when no longer in range
             LastInteractable = colliders[0];
         }
-        else if (LastInteractable != null)
+        else if (LastInteractable != null) // avoids null reference exception
         {
             highlight = LastInteractable.GetComponent<Highlight>();
             highlight.isHighlighted = false;
@@ -51,20 +53,9 @@ public class Interactor : MonoBehaviour
     // Used to see radius of interaction
     private void OnDrawGizmosSelected()
     {
-        
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(_interactionPoint.position, _interactionRadius);
-        /*
-        Handles.color = Color.red;
-        Handles.DrawWireDisc(_interactionPoint.position, new Vector3(0, 1, 0), _interactionRadius);
-        */
-    }
 
-    /*
-
-    bool InteractInput() {
-        return Input.GetKeyDown(KeyCode.E);
     }
-    */
 
 }
