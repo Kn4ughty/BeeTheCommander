25b49543addf4e3eef07b497e16dba8551e362d4
kn4ughty <Kn4ughty@users.noreply.github.com>
2023-05-22 10:08:11 +1000

    Reworked highlting script
    Now its a toggle using events, i will clean up code.
    Benifit of this system is that i can toggle it when its in the interaction range, makes it more flexible.

--- Assets/Scripts/Highlight.cs
+++ Assets/Scripts/Highlight.cs
@@ -8,19 +8,82 @@ public class Highlight : MonoBehaviour
     public float transitionTime = 1f;
     public float highlightBrightness = 1.5f; // Adjust this value to control the brightness
 
+    public bool isHighlighted;
+    private bool m_isHighlighted = false;
+
     private Color startColor;
     private Color targetColor;
 
     private Coroutine transitionCoroutine;
     private Renderer objectRenderer;
 
+
+    public delegate void OnVariableChangeDelegate(bool newVal);
+    public event OnVariableChangeDelegate OnVariableChange;
+
+    private void Update()
+    {
+        /*
+        if (isHighlighted)
+        {
+            transitionCoroutine = StartCoroutine(TransitionColor(highlightColour));
+        }
+        else 
+        {
+            transitionCoroutine = StartCoroutine(TransitionColor(startColor));
+        }
+        */
+        if (isHighlighted != m_isHighlighted && OnVariableChange != null)
+        {
+            m_isHighlighted = isHighlighted;
+            OnVariableChange(isHighlighted);
+            if (isHighlighted)
+            {
+                transitionCoroutine = StartCoroutine(TransitionColor(highlightColour));
+            }
+            else 
+            {
+                transitionCoroutine = StartCoroutine(TransitionColor(startColor));
+            }
+        }
+    }
+
+    void HandleVariableChange(bool newVal)
+    {
+    // Handle the variable change here
+    // You can access the new value through the newVal parameter
+    // ...
+    }
+
+
     // ChatGPT
 
     private void Start() {
         objectRenderer = GetComponent<Renderer>();
         startColor = objectRenderer.material.color;
+        OnVariableChange += HandleVariableChange;
     }
-    
+    // hellooooo i love youuuuuuu pretty girlll
+
+    private void OnMouseEnter()
+    {
+
+        if (transitionCoroutine != null)
+            StopCoroutine(transitionCoroutine);
+
+        isHighlighted = true;
+
+    }
+
+    private void OnMouseExit()
+    {
+        if (transitionCoroutine != null)
+            StopCoroutine(transitionCoroutine);
+
+        //transitionCoroutine = StartCoroutine(TransitionColor(startColor));
+        isHighlighted = false;
+    }
+    /*
     private void OnMouseEnter()
     {
 
@@ -37,6 +100,7 @@ public class Highlight : MonoBehaviour
 
         transitionCoroutine = StartCoroutine(TransitionColor(startColor));
     }
+    */
 
     private IEnumerator TransitionColor(Color targetColor)
     {
