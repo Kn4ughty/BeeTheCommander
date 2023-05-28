4b024d681a339b5ec2a288c6b73659acd0acde92
OneYellowJumper <134019263+OneYellowJumper@users.noreply.github.com>
2023-05-28 11:18:09 +1000

    working on inventory
    inventory

---
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    [SerializeField]
+    private InventoryPage inventoryUI;
+
+    public int InventorySize = 1;
+
+    private void Start()
+    {
+        inventoryUI.InitializeInventoryUI(InventorySize);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (inventoryUI.isActiveAndEnabled == false)
+            {
+                inventoryUI.Show();
+            }
+            else
+            {
+                inventoryUI.Hide();
+            }
+
+        }
+    }
+}
