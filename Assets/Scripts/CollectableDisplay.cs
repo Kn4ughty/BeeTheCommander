b36d01d7f1f36c539f00c255033738eb1b0e22eb
OneYellowJumper <134019263+OneYellowJumper@users.noreply.github.com>
2023-07-20 21:35:49 +1000

    added my gui. it is different, but it works.
    hehehehe WOMP WOMP

---
+++ Assets/Scripts/CollectableDisplay.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableDisplay : MonoBehaviour
+{
+    private int pollen = 0;
+    private int wood = 0;
+    private int water = 0;
+    private int stone = 0;
+
+    [SerializeField] private Text pollenText;
+    [SerializeField] private Text woodText;
+    [SerializeField] private Text waterText;
+    [SerializeField] private Text stoneText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pollen"))
+        {
+            Destroy(collision.gameObject);
+            pollen++;
+            pollenText.text = "Pollen: " + pollen;
+        }
+       if (collision.gameObject.CompareTag("Wood"))
+        {
+            Destroy(collision.gameObject);
+            wood++;
+            woodText.text = "Wood: " + wood;
+        }
+        if (collision.gameObject.CompareTag("Water"))
+        {
+            Destroy(collision.gameObject);
+            water++;
+            waterText.text = "Water: " + water;
+        }
+        if (collision.gameObject.CompareTag("Stone"))
+        {
+            Destroy(collision.gameObject);
+            stone++;
+            stoneText.text = "Stone: " + stone;
+        }
+    }
+}
