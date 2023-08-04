a77841b8f50d750414b8bd7d43d0e7bb181fa01f
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2023-08-04 13:39:25 +1000

    Github button

---
+++ Assets/openGitHub.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openGitHub : MonoBehaviour
+{
+
+    public void openGithub() {
+        Application.OpenURL("https://github.com/Kn4ughty/BeeTheCommander");
+    }
+}
